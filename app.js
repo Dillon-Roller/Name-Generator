1bdf056ca945b73384b83824a57da258c09fb6af
Dillon-Roller <dillonroller.18@gmail.com>
2021-03-25 02:13:44 -0700

    successfully change themes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const switcher = document.getElementById('theme');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+    var className = document.body.className;
+    console.log('current class name: ' + className);
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+}); 
