1164506c521bc75e0345bf7097e7ca0a3c91a335
Dillon-Roller <dillonroller.18@gmail.com>
2021-03-25 17:54:54 -0700

    feat: character to integer for mapping letters to transition matrix indices

--- markov_chain.js
+++ markov_chain.js
@@ -1,4 +1,9 @@
 //Create object for Markov Chain
 function MarkovChain() {
-  this.transitions = [] 
+  this.transitions = [];
+  
+  this.ctoi = function(c) {
+    c.toLowerCase()
+    c.charCodeAt(0) - 97
+  }
 }
