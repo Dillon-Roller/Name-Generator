e1709a3602be4df01e7ad40d8ab3f685572c7273
Dillon-Roller <dillonroller.18@gmail.com>
2021-03-26 13:49:56 -0700

    TODO: convert frequency to probabilities

--- markov_chain.js
+++ markov_chain.js
@@ -3,19 +3,34 @@
 class MarkovChain {
   #transitions;
   #names;
-  #startLetterCounts;
-  #endLetterCounts;
   #letterCounts;
 
   constructor() {
-    //fill arrays
+    this.fillArrays();
+  }
+
+  //tested
+  fillArrays() {
+    this.#transitions = this.zeros([26, 26]);
+    console.log(this.#transitions);
+  }
+
+  zeros(dimensions) {
+    var array = [];
+    for (var i = 0; i < dimensions[0]; ++i) {
+      array.push(dimensions.length == 1 ? 0 : this.zeros(dimensions.slice(1)));
+    }
+    return array;
   }
 
   //not tested
   static charToInt(c) {
-    result = c.toLowerCase().charCodeAt(0) - 97
-    console.log(c + "-->" + result)
-    return result;
+    if(c == " ") {
+      return 26; //blank character is end state for chain
+    }
+    else {
+      return c.toLowerCase().charCodeAt(0) - 97;
+    }
   }
 
   //not tested
@@ -25,8 +40,18 @@ class MarkovChain {
 
   //not tested
   updateTransitions() {
+    for(let i = 0; i < this.#names.length - 1; i++) {
+      this.update(this.#names[i], this.#names[i + 1]);
+    }
+    //last letter always goes to end state
+    this.update(this.#names[this.#names.length - 1], " ");
 
+    //convert frequency to probabilities
   };
+
+  update(from, to) {
+    this.#transitions[charToInt(from)][charToInt(to)]++;
+  }
 
   setNames(s) {
     this.#names = s.replace(/,/g, " ");
@@ -50,5 +75,5 @@ let markov = new MarkovChain();
 //works
 $("#namesButton").click(function() {
   markov.setNames($("#namesTextBox").val());
-  console.log("Names: " + markov.getNames());
+  console.log(`Names: ${markov.getNames()}`);
 });
